1a5e5c9fd652a0618aa234fb29a522f4143be73a
zhaohai1299002788 <zhaohaihzb@126.com>
2018-09-23 23:10:48 +0800

    提交项目

--- springboot-datacenter/src/main/java/com/example/datacenter/springbootdatacenter/SpringbootDatacenterApplication.java
+++ springboot-datacenter/src/main/java/com/example/datacenter/springbootdatacenter/SpringbootDatacenterApplication.java
@@ -3,6 +3,9 @@ package com.example.datacenter.springbootdatacenter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author
+ */
 @SpringBootApplication
 public class SpringbootDatacenterApplication {
 
---
+++ springboot-datacenter/src/main/java/com/example/datacenter/springbootdatacenter/service/TestController.java
@@ -0,0 +1,29 @@
+package com.example.datacenter.springbootdatacenter.service;
+
+import com.example.springbootutils.utils.DateUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author
+ */
+@RestController
+public class TestController {
+
+    /**
+     * 测试获取时间
+     *
+     * @return
+     */
+    @GetMapping("date")
+    public String getDate() {
+        Date date = DateUtils.getDate();
+        // 设置日期格式
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return "当前时间 ： " + simpleDateFormat.format(date);
+    }
+
+}
